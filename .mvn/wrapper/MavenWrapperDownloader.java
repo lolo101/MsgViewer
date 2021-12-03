8f7a7e8656e75661ddf9ef1ec0c7f63b3ffce7df
Khashayar Etemadi <khaes@kth.se>
2021-12-03 21:53:52 +0100

    Fix violations of Sonar rule 2095

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -108,10 +108,11 @@ public class MavenWrapperDownloader {
         URL website = new URL(urlString);
         ReadableByteChannel rbc;
         rbc = Channels.newChannel(website.openStream());
-        FileOutputStream fos = new FileOutputStream(destination);
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        fos.close();
-        rbc.close();
+        try (FileOutputStream fos = new FileOutputStream(destination)) {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            fos.close();
+            rbc.close();
+        }
     }
 
 }
