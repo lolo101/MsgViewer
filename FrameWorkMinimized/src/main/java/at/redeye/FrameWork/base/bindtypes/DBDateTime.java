ed72647e62efbd49c5b7f9b3d8ecd720d7fa159d
lolo101 <lbroquet@online.fr>
2022-09-25 17:48:53 +0200

    Cleanup dead code

--- FrameWorkMinimized/src/main/java/at/redeye/FrameWork/base/bindtypes/DBDateTime.java
+++ FrameWorkMinimized/src/main/java/at/redeye/FrameWork/base/bindtypes/DBDateTime.java
@@ -5,9 +5,6 @@ import at.redeye.SqlDBInterface.SqlDBIO.impl.DBDataType;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.regex.Pattern;
 
@@ -16,9 +13,6 @@ public class DBDateTime extends DBValue {
     private static final SimpleDateFormat SDF_4_STD_STRING = new SimpleDateFormat(StmtExecInterface.SQLIF_STD_DATE_FORMAT + " "
             + StmtExecInterface.SQLIF_STD_TIME_FORMAT);
 
-    private static final SimpleDateFormat SDF_4_SQLIF_STD_TIME_FORMAT = new SimpleDateFormat(StmtExecInterface.SQLIF_STD_TIME_FORMAT);
-
-    private static final SimpleDateFormat SDF_4_SQLIF_STD_DATE_FORMAT = new SimpleDateFormat(StmtExecInterface.SQLIF_STD_DATE_FORMAT);
     private static final Pattern TIME_PATTERN = Pattern.compile("(([0-1][0-9])|(2[0-3])):[0-5][0-9]:[0-5][0-9]");
 
     protected Date value = new Date(0);
@@ -93,37 +87,6 @@ public class DBDateTime extends DBValue {
         return SDF_4_STD_STRING.format(date);
     }
 
-    public static String getStdString(LocalDateTime date) {
-        return date.format(DateTimeFormatter.ofPattern(StmtExecInterface.SQLIF_STD_DATE_FORMAT + " "
-                + StmtExecInterface.SQLIF_STD_TIME_FORMAT));
-    }
-
-    public static String getStdString(LocalDate date) {
-        LocalDateTime dt = date.atStartOfDay();
-        return dt.format(DateTimeFormatter.ofPattern(StmtExecInterface.SQLIF_STD_DATE_FORMAT + " "
-                + StmtExecInterface.SQLIF_STD_TIME_FORMAT));
-    }
-
-
-    public String getTimeStr() {
-        return getTimeStr(value);
-    }
-
-
-    public static String getTimeStr(Date date) {
-
-        return SDF_4_SQLIF_STD_TIME_FORMAT.format(date);
-
-    }
-
-    public String getDateStr() {
-        return getDateStr(value);
-    }
-
-    public static String getDateStr(Date value) {
-
-        return SDF_4_SQLIF_STD_DATE_FORMAT.format(value);
-    }
 
     public boolean loadTimePart(String time) {
         if (!TIME_PATTERN.matcher(time).matches())
