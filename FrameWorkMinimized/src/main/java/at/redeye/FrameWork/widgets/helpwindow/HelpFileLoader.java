c00942d8e7c0d4fa25e57c0b9185e970e0fc38db
lolo101 <lbroquet@online.fr>
2023-02-14 01:31:24 +0100

    Fix #165

--- FrameWorkMinimized/src/main/java/at/redeye/FrameWork/widgets/helpwindow/HelpFileLoader.java
+++ FrameWorkMinimized/src/main/java/at/redeye/FrameWork/widgets/helpwindow/HelpFileLoader.java
@@ -10,102 +10,43 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class HelpFileLoader {
 
 	private static final Logger logger = LogManager.getLogger(HelpFileLoader.class);
-
-	private static int findImgTag(String s) {
-		int pos;
-		int start = 0;
-
-		do {
-			pos = s.indexOf("img", start);
-
-			if (pos <= 0) {
-				// weil bei start 0 getht sich keine <img aus
-				return -1;
-			}
-
-			int rpos = StringUtils.skip_spaces_reverse(s, pos - 1);
-
-			if (rpos < 0) {
-				start = pos + 3;
-				continue;
-			}
-
-			if (s.charAt(rpos) != '<') {
-				start = pos + 3;
-				continue;
-			}
-
-			if (s.length() <= pos + 3)
-				return -1;
-
-			if (StringUtils.is_space(s.charAt(pos + 3)))
-				return pos;
-		} while (pos < (s.length() - 1));
-
-		return -1;
-	}
+	private static final Pattern IMG_TAG = Pattern.compile("<img\\s+");
+	private static final Pattern SRC_ATTRIBUTE = Pattern.compile("src\\s*=\\s*\"(?<value>.*?)\"");
 
 	private String replace_src(String s) {
-
-		List<String> res = StringUtils.split_str(s, "=");
-
-		int i = 0;
-
-		StringBuilder ret = new StringBuilder();
-
-		for (String part : res) {
-			i++;
-
-			if (i == 1) {
-				ret.append(part);
-				continue;
-			}
-
-			ret.append("=");
-
-			String src = StringUtils
-					.strip(part, " \t\r\n\"");
-
-			URL resource = getClass().getResource(src);
-
-			String imgsrc = null;
-
-			if (resource != null) {
-				imgsrc = resource.toString();
-				logger.info("image: " + src);
-			} else {
-				logger.error("Cannot load Image: " + src);
-			}
-
-			if (imgsrc != null) {
-				ret.append("\"");
-				ret.append(imgsrc);
-				ret.append("\"");
-			}
+		String src = StringUtils.strip(s, " \t\r\n\"");
+		URL resource = getClass().getResource(src);
+		if (resource == null) {
+			logger.error("Cannot load Image: " + src);
+			return s;
 		}
-
-		return ret.toString();
+		logger.info("image: " + src);
+		return resource.toString();
 	}
 
 	private String prepareImages(String s) {
 		StringBuilder res = new StringBuilder(s);
-		for (int start = findImgTag(s); start >= 0; ) {
-			int end = s.indexOf('>', start);
 
-			if (end < 0)
-				break;
+		int tagFromIndex = 0;
+		for (Matcher imgMatcher = IMG_TAG.matcher(res); imgMatcher.find(tagFromIndex); ) {
+			int attributeFromIndex = imgMatcher.end();
+			Matcher srcMatcher = SRC_ATTRIBUTE.matcher(res);
+			if (srcMatcher.find(attributeFromIndex)) {
+				int attributeValueStart = srcMatcher.start("value");
+				int attributeValueEnd = srcMatcher.end("value");
 
-			String substitute = replace_src(res.substring(start, end));
+				String substitute = replace_src(res.substring(attributeValueStart, attributeValueEnd));
 
-			res.replace(start, end, substitute);
-
-			start += substitute.length();
+				res.replace(attributeValueStart, attributeValueEnd, substitute);
+			}
+			tagFromIndex = attributeFromIndex;
 		}
 
 		return res.toString();
