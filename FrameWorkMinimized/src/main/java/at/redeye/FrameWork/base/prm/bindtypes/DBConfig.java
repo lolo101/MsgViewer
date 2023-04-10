8a7871fb3cd097c0e9ac5d9ec664d3c3f0e3e1a6
lolo101 <lbroquet@online.fr>
2023-04-10 17:53:32 +0200

    slight refactor

--- FrameWorkMinimized/src/main/java/at/redeye/FrameWork/base/prm/bindtypes/DBConfig.java
+++ FrameWorkMinimized/src/main/java/at/redeye/FrameWork/base/prm/bindtypes/DBConfig.java
@@ -19,10 +19,10 @@ public class DBConfig extends DBStrukt implements PrmAttachInterface {
 
     private PrmDefaultChecksInterface defaultChecks;
 
-    public DBString name = new DBString("name", "Name", 100);
-    public DBString value = new DBString("value", "Wert", 100);
-    public DBString descr = new DBString("description", "Beschreibung", 250);
-    public DBHistory hist = new DBHistory("hist");
+    public final DBString name = new DBString("name", "Name", 100);
+    public final DBString value = new DBString("value", "Wert", 100);
+    public final DBString descr = new DBString("description", "Beschreibung", 250);
+    public final DBHistory hist = new DBHistory("hist");
 
     public DBConfig() {
         super(TABLENAME);
@@ -30,8 +30,7 @@ public class DBConfig extends DBStrukt implements PrmAttachInterface {
         register();
     }
 
-    public DBConfig( String name, String value, String descr )
-    {
+    public DBConfig(String name, String value, String descr) {
         super(TABLENAME);
 
         register();
@@ -112,20 +111,17 @@ public class DBConfig extends DBStrukt implements PrmAttachInterface {
     @Override
     public void updateListeners(PrmActionEvent prmActionEvent) {
 
-        System.out.println("PRM " + name.toString() +
+        System.out.println("PRM " + name +
                 " has " + prmListeners.size() + " listener(s) registered!");
 
         boolean valueHasChanged = prmActionEvent.valueHasChanged();
 
         for (PrmListener currentListener : prmListeners) {
-            if (defaultChecks != null) {
-                if (valueHasChanged)
-                    currentListener.onChange(defaultChecks, prmActionEvent);
-            } else {
+            if (defaultChecks == null) {
                 System.out.println("-> no default check");
+            } else if (valueHasChanged) {
+                currentListener.onChange(defaultChecks, prmActionEvent);
             }
-
-            System.out.println("-> no custom check");
         }
     }
 
