6cfdec4ff91f8821825b5810fc083eec823327dc
lolo101 <lbroquet@online.fr>
2023-10-21 15:07:58 +0200

    add test case for unnamed attachments

--- MSGViewer/src/test/java/net/sourceforge/MSGViewer/factory/mbox/JavaMailParserTest.java
+++ MSGViewer/src/test/java/net/sourceforge/MSGViewer/factory/mbox/JavaMailParserTest.java
@@ -1,12 +1,17 @@
 package net.sourceforge.MSGViewer.factory.mbox;
 
 import com.auxilii.msgparser.Message;
+import com.auxilii.msgparser.attachment.Attachment;
+import com.auxilii.msgparser.attachment.FileAttachment;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.net.URI;
 import java.nio.file.Path;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JavaMailParserTest {
@@ -19,5 +24,19 @@ class JavaMailParserTest {
         Message message = sut.parse();
         String bodyText = message.getBodyText();
         assertTrue(bodyText.contains(expected));
+    }
+
+    @Test
+    void unnamed_attachments_should_be_named_unknown() throws Exception {
+        URI uri = JavaMailParserTest.class.getResource("/unnamed-attachment.eml").toURI();
+        Path file = Path.of(uri);
+        JavaMailParser sut = new JavaMailParser(file);
+        Message message = sut.parse();
+        List<Attachment> attachments = message.getAttachments();
+        assertThat(attachments)
+                .element(0)
+                .isInstanceOf(FileAttachment.class)
+                .extracting(attachment -> ((FileAttachment) attachment).getLongFilename())
+                .isEqualTo("unknown");
     }
 }
