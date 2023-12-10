c1858ddbcfdd171c73df4acabd495927b037bfa7
lolo101 <lbroquet@online.fr>
2023-12-11 00:00:35 +0100

    update modules

--- msgparser/src/main/java/module-info.java
+++ msgparser/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module msgparser {
-    requires commons.validator;
-    requires org.apache.commons.lang3;
-    requires org.apache.logging.log4j;
     requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.validator;
+    requires org.apache.logging.log4j;
     requires transitive org.apache.poi.scratchpad;
 
     exports com.auxilii.msgparser;
