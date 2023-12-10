2f89f719767f1f488f55b0ec132dd38786372306
lolo101 <lbroquet@online.fr>
2023-12-11 00:00:35 +0100

    update modules

--- MSGViewer/src/main/java/module-info.java
+++ MSGViewer/src/main/java/module-info.java
@@ -3,9 +3,9 @@ module msgviewer {
     requires msgparser;
     requires jericho.html;
     requires jakarta.mail;
-    requires org.apache.commons.lang3;
     requires org.apache.commons.codec;
     requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.validator;
     requires org.apache.logging.log4j;
-    requires commons.validator;
 }
