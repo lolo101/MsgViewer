2a44308c91f4a4bf37d9a2d03abaecce7b5cb50b
lolo101 <lbroquet@online.fr>
2026-01-04 20:02:52 +0100

    Fix #411. Default LookAndFeel is now the cross-platform "Nimbus" instead of "System"

--- FrameWorkMinimized/src/main/java/at/redeye/FrameWork/base/FrameWorkConfigDefinitions.java
+++ FrameWorkMinimized/src/main/java/at/redeye/FrameWork/base/FrameWorkConfigDefinitions.java
@@ -20,9 +20,11 @@ public class FrameWorkConfigDefinitions {
     public static final DBConfig SpreadSheetMarginEditable = new DBConfig("SpreadSheetMarginEditable", "20", "Zusätzlicher Rand, um den die Tabellenspalten breiter gemacht werden um besseres Editieren zu ermöglichen. Dies gilt nur für editierbare Spalten.");
     public static final DBConfig SpreadSheetMarginReadOnly = new DBConfig("SpreadSheetMarginReadOnly", "5", "Zusätzlicher Rand, um den die Tabellenspalten breiter gemacht werden. Dieser Wert gilt nur für nicht editierbare Spalten.");
     public static final DBConfig DefaultAutoLineBreakWidth = new DBConfig("DefaultAutoLineBreakWidth", "40", "Breite eines automatisch umgebrochenen Textes.", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_LONG));
-    public static final DBConfig LookAndFeel = new DBConfig("LookAndFeel", "System", "Bestimmt das Aussehen der Benutzoberfläche. Mögliche Werte sind \"System\", \"Motif\", \"Nimbus\" oder \"Metal\"",
+    private static final String[] POSSIBLE_LOOK_AND_FEEL = Arrays.stream(UIManager.getInstalledLookAndFeels()).map(UIManager.LookAndFeelInfo::getName).toArray(String[]::new);
+    public static final DBConfig LookAndFeel = new DBConfig("LookAndFeel", "Nimbus",
+            "Bestimmt das Aussehen der Benutzoberfläche. Mögliche Werte sind " + Arrays.toString(POSSIBLE_LOOK_AND_FEEL),
             new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_LOOKANDFEEL | PrmDefaultChecksInterface.PRM_HAS_VALUE),
-            Arrays.stream(UIManager.getInstalledLookAndFeels()).map(UIManager.LookAndFeelInfo::getName).toArray(String[]::new));
+            POSSIBLE_LOOK_AND_FEEL);
     public static final DBConfig SpreadSheetRowHeaderLimit = new DBConfig("SpreadSheetRowHeaderLimit", "20", "Legt fest ab welcher Anzahl von Zeilen im Spreadsheet die Zeilennummer eingeblendet werden sollen.", new PrmDefaultCheckSuite(PrmDefaultChecksInterface.PRM_IS_LONG));
 
     public static final DBConfig OpenCommand = new DBConfig("OpenCommand", "xdg-open", "Kommando für das öffnen einer Datei, oder eines Verzeichnisses");
